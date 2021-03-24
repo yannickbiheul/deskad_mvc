cde1abe55385f5df86e1c21d35a79116749f9289
Yannick Biheul <yannickbiheul@outlook.fr>
2021-03-24 19:56:01 +0100

    viewHome terminé

--- index.php
+++ index.php
@@ -1,72 +1,24 @@
-<!DOCTYPE html>
-<html lang='fr'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Template</title>
-    <!-- DESCRIPTION -->
-    <meta name='description' content='Un template en scss'>
-    <!-- CSS -->
-    <link rel='stylesheet' href='assets/css/main.css'>
-    <!-- GOOGLE FONTS -->
-    <!-- Roboto -->
-    <link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>
-    <!-- Quicksand -->
-    <link rel='preconnect' href='https://fonts.gstatic.com'>
-    <link href='https://fonts.googleapis.com/css2?family=Quicksand&display=swap' rel='stylesheet'>
-    <!-- Bangers -->
-    <link rel='preconnect' href='https://fonts.gstatic.com'>
-    <link href='https://fonts.googleapis.com/css2?family=Bangers&display=swap' rel='stylesheet'>
-    <!-- FONTAWESOME -->
-    <script src='https://kit.fontawesome.com/29ef46100e.js' crossorigin='anonymous'></script>
-    <!-- jQuery -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-</head>
+<?php
 
-<body>
+require_once('controllers/Controller.php');
+$controller = new Controller;
 
-    <header>
-        <h1>Deskad</h1>
-        <div class="mobileButton">
-        </div>
-    </header>
+if (isset($_GET['action']) && isset($_GET['params'])) {
 
-    <aside>
+    if ($_GET['action'] == 'home' && isset($_GET['params'])) {
+        $controller->home();
+    }
 
-        <nav>
-            <a href="#">Accueil</a>
-            <a href="#">Articles</a>
-            <?php
-                if (isset($_SESSION['email']) && $_SESSION['email'] == 'yannickbiheul@outlook.fr') {
-                    ?>
-                        <a href="#">Créer article</a>
-                    <?php
-                }
-            ?>
-            <a href="#">À propos</a>
-            <a href="#">Contact</a>
-            <a href="#">Connexion</a>
-        </nav>
+} else if (isset($_GET['action'])) {
 
-        <div class="socials">
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-github"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            <a href="#"><i class="fab fa-youtube"></i></a>
-        </div>
+    if ($_GET['action'] == 'home') {
+        $controller->home();
+    }
 
-    </aside>
+    if ($_GET['action'] == 'connect') {
+        $controller->connect();
+    }
 
-    <main>
-    </main>
-
-    <footer>
-        <p>©2021 - Yannick Biheul</p>
-    </footer>
-
-    <!-- JAVASCRIPT -->
-    <script src='js/main.js'></script>
-</body>
-</html>
+} else {
+    $controller->home();
+}
